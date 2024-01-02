d4edaa02b47f059cd11511353143823347c63003
sir-njuguna <sir.njuguna@outlook.com>
2024-01-02 13:36:28 +0300

    Code refactoring: Renamed files from MessageController.java to GreetingsController.java.

--- src/main/java/com/example/app/config/MessageSourceConfig.java
+++ src/main/java/com/example/app/config/MessageSourceConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @Configuration
-public class MessageConfig {
+public class MessageSourceConfig {
 
     @Bean
     public MessageSource messageSource(){
